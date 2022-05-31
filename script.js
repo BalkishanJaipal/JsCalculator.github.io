d2f3ba375bcd229ac1b336cefd8707e6c607abc9
Balkishan Jaipal <98956626+BalkishanJaipal@users.noreply.github.com>
2022-05-31 23:04:53 +0530

    1

---
+++ script.js
@@ -0,0 +1,16 @@
+function display(value)
+{
+    document.getElementById('text-area').value += value; /*appending the value*/
+}
+
+function calculate()
+{
+    var value = document.getElementById('text-area').value;
+    var answer = eval(value);
+    document.getElementById('text-area').value += ' = '+answer;
+}
+
+function clr()
+{
+    document.getElementById('text-area').value = '';
+}
